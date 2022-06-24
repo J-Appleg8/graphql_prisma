bb08d385ce09b61172b083a444b8d694a8d74011
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2022-06-24 13:59:33 -0700

    added node files from starter sections of course - set up prisma

---
+++ src/db.js
@@ -0,0 +1,79 @@
+// Database Data
+const users = [
+  {
+    id: '1',
+    name: 'James',
+    email: 'james@example.com',
+    age: 25,
+  },
+  {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com',
+  },
+  {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com',
+  },
+];
+
+const posts = [
+  {
+    id: '1',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL',
+    published: true,
+    author: '1',
+  },
+  {
+    id: '2',
+    title: 'GraphQL 200',
+    body: 'This is an advanced GraphQL post',
+    published: false,
+    author: '1',
+  },
+  {
+    id: '3',
+    title: 'Programming Music',
+    body: '',
+    published: true,
+    author: '2',
+  },
+];
+
+const comments = [
+  {
+    id: '1',
+    text: 'This worked well for me. Thanks!',
+    author: '3',
+    post: '1',
+  },
+  {
+    id: '2',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '1',
+  },
+  {
+    id: '3',
+    text: 'This did not work',
+    author: '2',
+    post: '2',
+  },
+  {
+    id: '4',
+    text: 'Nevermind. I got it to work',
+    author: '1',
+    post: '3',
+  },
+];
+
+//////////////////////////////////////////////////
+const db = {
+  users,
+  posts,
+  comments,
+};
+
+export { db as default };
